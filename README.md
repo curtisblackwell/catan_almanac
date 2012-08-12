8560cda6535a1648677edc9fa6085300c018bea7
Curtis Blackwell <blackwell.curtis@gmail.com>
2012-08-12 16:22:50 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# [Catan Almanac](http://curtisblackwell.github.com/catan_almanac)
+
+Catan Almanac is a reference intended to aid English-speakers when playing the Korean version of the game
+[*The Settlers of Catan*](http://www.catan.com/).
---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function() {
+
+
+  $('#nav_progress li a').click(function(){
+    // Remove the active class from tab and add to the one that was clicked.
+    $('#nav_progress .active').removeClass('active');
+    $(this).parent().addClass('active');
+
+    // Hide the current visible realm and show the one whose tab was clicked.
+    var active_realm = $(this).attr('href');
+
+    $('.realm.active').fadeOut(function(){
+      $(this).removeClass('active');
+      $(active_realm).fadeIn(function(){
+        $(this).addClass('active');
+      });
+    });
+
+    return false; // Don't jump down the page or append to the URL.
+  });
+
+  $('.card').click(function(){
+    $(this).toggleClass('active');
+  })
+
+
+});
